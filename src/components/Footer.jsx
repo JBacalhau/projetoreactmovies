e33e6cd8ec8c92f507ddc98b0dfc197b849b7204
João Bacalhau <joao.bacalhau81@gmail.com>
2024-09-30 15:24:21 -0300

    Alteração de informação do head

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export function Footer() {
 
 
         <footer className="text-center py-4 bg-brand-blue-900 ">
-            <p>Projeto desenvolvido por: Coti Informática</p>
+            <p>Projeto desenvolvido por: João Bacalhau</p>
         </footer>
 
     )
